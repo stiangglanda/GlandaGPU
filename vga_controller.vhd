bda0e2be188810a4972659f13569b417aa0d3e63
Leander Kieweg <kieweg.leander@gmail.com>
2026-02-19 15:25:15 +0100

    Refactor code structure for improved readability and maintainability

--- vga_controller.vhd
+++ vga_controller.vhd
@@ -37,8 +37,15 @@ architecture Behavioral of vga_controller is
     -- Counters
     signal h_cnt : integer range 0 to H_TOTAL - 1 := 0;
     signal v_cnt : integer range 0 to V_TOTAL - 1 := 0;
-	 
-	signal video_on_int : std_logic;
+
+    -- fix for delay problem of VRAM
+    signal hsync_i    : std_logic;
+    signal vsync_i    : std_logic;
+    signal video_on_i : std_logic;
+
+    signal hsync_v1, hsync_v2       : std_logic := '1';
+    signal vsync_v1, vsync_v2       : std_logic := '1';
+    signal video_on_v1, video_on_v2 : std_logic := '0';
 
 begin
 
@@ -64,45 +71,51 @@ begin
         end if;
     end process;
 
-    -- Sync Signal Generation
+    -- Sync Signale berechnen Generation
+    hsync_i <= '0' when (h_cnt >= (H_ACTIVE + H_FP)) and (h_cnt < (H_ACTIVE + H_FP + H_SYNC)) else '1';
+    vsync_i <= '0' when (v_cnt >= (V_ACTIVE + V_FP)) and (v_cnt < (V_ACTIVE + V_FP + V_SYNC)) else '1';
+
+    -- video_on Signal
+    video_on_i <= '1' when (h_cnt < H_ACTIVE) and (v_cnt < V_ACTIVE) else '0';
+    -- Calculate Pixeladdresse
+    pixel_addr <= std_logic_vector(to_unsigned(v_cnt * 640 + h_cnt, 19)) 
+                  when (video_on_i = '1') else (others => '0');
+
+    -- Delay hsync, vsync und video_on for 2 Takte, so they are in sync with the PixelData from VRAM
     process(clk)
     begin
         if rising_edge(clk) then
-            -- Horizontal Sync
-            if (h_cnt >= (H_ACTIVE + H_FP)) and (h_cnt < (H_ACTIVE + H_FP + H_SYNC)) then
-                hsync <= '0';
-            else
-                hsync <= '1';
-            end if;
-
-            -- Vertical Sync
-            if (v_cnt >= (V_ACTIVE + V_FP)) and (v_cnt < (V_ACTIVE + V_FP + V_SYNC)) then
-                vsync <= '0';
-            else
-                vsync <= '1';
-            end if;
+            -- in Sync with VRAM output
+            hsync_v1    <= hsync_i;
+            vsync_v1    <= vsync_i;
+            video_on_v1 <= video_on_i;
++
+            -- in sync with the RGB output
+            hsync_v2    <= hsync_v1;
+            vsync_v2    <= vsync_v1;
+            video_on_v2 <= video_on_v1;
         end if;
     end process;
-    
-    -- Video On
-	video_on_int <= '1' when (h_cnt < H_ACTIVE) and (v_cnt < V_ACTIVE) else '0';
-	video_on <= video_on_int;
 
-    -- Read pixel from VRAM
-    pixel_addr <= std_logic_vector(to_unsigned(v_cnt * 640 + h_cnt, 19)) 
-              when (video_on_int = '1') else (others => '0');
-
+    -- Fetch Pixel Data from VRAM
     process(clk)
     begin
         if rising_edge(clk) then
-            if video_on_int = '1' then
-                red   <= pixel_data(11 downto 8); -- TODO delay problem pixel kommt erst im nächsten takt
-                green <= pixel_data(7 downto 4);  -- erster Pixel ist immer schwarz, da pixel_data erst im nächsten Takt gültig ist
+            -- use v1 so it is in sync with the VRAM output
+            if video_on_v1 = '1' then
+                red   <= pixel_data(11 downto 8);
+                green <= pixel_data(7 downto 4);
                 blue  <= pixel_data(3 downto 0);
             else
-                red <= "0000"; green <= "0000"; blue <= "0000";
+                red <= "0000"; 
+                green <= "0000"; 
+                blue <= "0000";
             end if;
         end if;
     end process;
+
+    hsync    <= hsync_v2;
+    vsync    <= vsync_v2;
+    video_on <= video_on_v2;
 
 end Behavioral;
