1314bffaf9e9d9fb4dbb4fb6803585a887cd0eba
Leander Kieweg <leander.kieweg@gmail.com>
2026-05-01 23:30:27 +0200

    Add initial run.py script to set up VUnit with source files and testbench

---
+++ run.py
@@ -0,0 +1,20 @@
+from vunit import VUnit
+
+# Create VUnit instance using QuestaSim
+vu = VUnit.from_argv(compile_builtins=True)
+vu.add_vhdl_builtins()
+
+# Create a library (replaces your 'work' lib)
+lib = vu.add_library("lib")
+
+# Add all source files
+lib.add_source_files("src/gpu_engine.vhd")
+lib.add_source_files("src/gpu_regs.vhd")
+lib.add_source_files("src/vram.vhd")
+lib.add_source_files("src/vga_controller.vhd")
+lib.add_source_files("src/top_gpu.vhd")
+
+# Add testbench
+lib.add_source_files("src/tb_vga.vhd")
+
+vu.main()
