afafb9802eee0b1660164529cc40aedbc18493f1
Leander Kieweg <leander.kieweg@gmail.com>
2026-05-02 00:50:55 +0200

    Add conditional environment variable setup for VUnit in run.py

--- run.py
+++ run.py
@@ -2,8 +2,9 @@ import os
 
 from vunit import VUnit
 
-os.environ["VUNIT_MODELSIM_INI"] = "sim/modelsim.ini"
-os.environ["VUNIT_MODELSIM_PATH"] = "/home/stiangglanda/altera_lite/25.1std/questa_fse/bin"
+if os.environ.get("GITHUB_ACTIONS") != "true":
+    os.environ["VUNIT_MODELSIM_INI"] = "sim/modelsim.ini"
+    os.environ["VUNIT_MODELSIM_PATH"] = "/home/stiangglanda/altera_lite/25.1std/questa_fse/bin"
 
 # Create VUnit instance using QuestaSim
 vu = VUnit.from_argv(compile_builtins=False)
